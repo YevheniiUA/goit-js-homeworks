c069993d6924248738f40b87726d8937d41f0e40
Evgenii <wojda007@gmail.com>
2019-05-28 20:54:08 +0300

    update

--- homeTasks/module-08-1/js/task.js
+++ homeTasks/module-08-1/js/task.js
@@ -2,11 +2,15 @@
 import quizData from "./quiz-data.js";
 
 const createQuiz = quizData => {
+  document.querySelector("button").classList.add("button");
   const form = document.querySelector("form");
+  form.classList.add("form");
   const quizSections = quizData.questions.reduce((acc, quiz, index) => {
     const quizSection = document.createElement("section");
+    quizSection.classList.add("form-sections");
 
     const quizTitle = document.createElement("h3");
+    quizTitle.classList.add("form-sections__title");
     quizTitle.textContent = `${index + 1}. ${quiz.question}`;
 
     quizSection.append(quizTitle, createQuizElement(quiz.choices, index));
@@ -16,8 +20,9 @@ const createQuiz = quizData => {
   form.insertAdjacentHTML("afterbegin", quizSections);
   const formTitle = document.createElement("h2");
   formTitle.textContent = quizData.title;
+  formTitle.classList.add("form-title");
   form.prepend(formTitle);
-  return checkQuiz(form,quizData.questions);
+  return checkQuiz(form, quizData.questions);
 };
 
 const createQuizElement = (choicesArr, number) => {
@@ -40,8 +45,10 @@ const createQuizElement = (choicesArr, number) => {
   return quizList;
 };
 
-const checkQuiz = (form,questions) => {
+const checkQuiz = (form, questions) => {
   const answer = document.createElement("h2");
+  answer.classList.add("answer");
+  document.querySelector("button").insertAdjacentElement("beforebegin", answer);
   form.addEventListener("submit", e => {
     e.preventDefault();
     let countCorrectAnswer = 0;
@@ -51,12 +58,10 @@ const checkQuiz = (form,questions) => {
         countCorrectAnswer += 1;
       }
     });
-    countCorrectAnswer*100/questions.length >= 80 ? answer.textContent = 'Удача' : answer.textContent = 'Неудача';
-    document
-      .querySelector("button")
-      .insertAdjacentElement("beforebegin", answer);
+    (countCorrectAnswer * 100) / questions.length >= 80
+      ? (answer.textContent = "Удача")
+      : (answer.textContent = "Неудача");
   });
 };
 
 createQuiz(quizData);
-
