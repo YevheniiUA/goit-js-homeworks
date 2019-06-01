6f0df1dde5e7dfa598333d15eb60f42f4b0cc1db
Evgenii <wojda007@gmail.com>
2019-06-01 10:23:25 +0300

    finish 3

--- addTasks/module-09/task.js
+++ addTasks/module-09/task.js
@@ -1,7 +1,30 @@
-const button = document.querySelector('button[data-action="add"]');
-button.addEventListener("click", handlerButton);
-function handlerButton() {
-  const numbers = document.querySelectorAll("input");
-  const result = document.querySelector(".result");
-  result.textContent = Number(numbers[0].value) + Number(numbers[1].value);
+class Counter {
+  constructor(value) {
+    this.value = value;
+  }
+  onChancge(value) {
+    const currentValue = document.querySelector(".value");
+    currentValue.textContent = value;
+  }
+  increment() {
+    this.value += 1;
+    this.onChancge(this.value);
+  }
+  decrement() {
+    this.value -= 1;
+    this.onChancge(this.value);
+  }
+}
+
+const cont = new Counter(0);
+const sub = document.querySelector('button[data-action="sub"]');
+const add = document.querySelector('button[data-action="add"]');
+sub.addEventListener("click", handlerSub);
+add.addEventListener("click", handlerAdd);
+
+function handlerSub(e) {
+  cont.decrement();
+}
+function handlerAdd(e) {
+  cont.increment();
 }
