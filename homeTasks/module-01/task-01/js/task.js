9c13d5099099d7cc4d70ac0918b86a2893887b6f
Evgenii <wojda007@gmail.com>
2019-10-27 20:28:20 +0200

    fix: update solution

--- homeTasks/module-01/task-01/js/task.js
+++ homeTasks/module-01/task-01/js/task.js
@@ -1,12 +1,10 @@
-const ADMIN_PASSWORD = 'm4ng0h4ckz';
-let message;
-const inputPassword = prompt('Введите пароль.');
+const ADMIN_PASSWORD = "m4ng0h4ckz";
+let message = "Добро пожаловать!";
+const inputPassword = prompt("Введите пароль.");
 
 if (inputPassword === null) {
-    message = 'Отменено пользователем!';
+  message = "Отменено пользователем!";
 } else if (inputPassword !== ADMIN_PASSWORD) {
-    message = 'Доступ запрещен, неверный пароль!';
-} else {
-    message = 'Добро пожаловать!';
+  message = "Доступ запрещен, неверный пароль!";
 }
 alert(message);
